00a808a5b7d20534f04676db792e0720da19a479
obrechal10 <ob.rechal1010@gmail.com>
2025-07-15 23:03:20 +0530

    Add files via upload

---
+++ TASK16Calculator.js
@@ -0,0 +1,14 @@
+a=750;
+b=15;
+sum=a+b;
+sub=a-b;
+multi=a*b;
+div=a/b;
+console.log("Addition: "+sum);
+console.log("subtractiion: "+sub);
+console.log("multiplication: "+multi);
+console.log("Division: "+div);
+
+
+
+
